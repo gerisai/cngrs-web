88b779422f46f524b6c85db931daa5530b62c95d
Gerardo Diaz <gerardoisai.diaz@gmail.com>
2024-07-29 19:04:11 -0600

    Add error message to error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,12 +1,14 @@
 import { Flex, Typography } from 'antd';
 
-const { Title } = Typography;
+const { Title, Text } = Typography;
 
-function Error () {
+function Error ({ message }) {
   return (
     <Flex vertical className='main-flex' align='center' justify='center'>
       <Title>500 Ocurrió un error en el servidor</Title>
       <img width='30%' src="/elmo_fire.gif" alt="ElmoOnFire" />
+      <Text type="danger">Mensaje confuso pal programador</Text>
+      <Text keyboard>{ message }</Text>
     </Flex>
   )
 }
