2cac385b930756aca05ab11529caa79c871fa6a6
Gerardo Diaz <gerardoisai.diaz@gmail.com>
2024-11-02 22:22:08 -0600

    feat: make header responsive

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { AppstoreOutlined, UserOutlined, TableOutlined, TeamOutlined, LogoutOutlined } from '@ant-design/icons';
-import { Layout, FloatButton, Button, Image, Typography, Avatar, Flex, Upload, Spin } from 'antd';
+import { Layout, FloatButton, Image, Typography, Avatar, Flex, Upload, Spin, Row, Col } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import { useMutation } from '@tanstack/react-query';
 // Project imports
@@ -65,12 +65,16 @@ const HomeLayout = ({ children }) => {
       <Layout>
       <Header className='header'>
           <Image alt='cnrgs-logo' preview={false} width={50} src='/CNGRS.svg' />
-          <Flex justify='center' align='center'>
+          <Row>
+            <Col sm={4}>
             <Upload {...avatarProps}>
               { avatarLoading ? <Spin size="large" /> : <Avatar shape="square" src={user.avatar} size="large" icon={<UserOutlined />} /> }
             </Upload>
-            <Title style={{ marginLeft: 10, marginTop: 20 }}>{user.name.split(' ')[0]}</Title>
-          </Flex>
+            </Col>
+            <Col xs={0} sm={20}>
+            <Title style={{ marginLeft: 10, marginTop: 10 }}>{user.name.split(' ')[0]}</Title>
+            </Col>
+          </Row>
       </Header>
         <Content style={{ margin: '24px 24px'}}>
           <div className='content'>
